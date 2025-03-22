e1d5253b804c28f1e566375e01f993751894d8d8
jiaopengzi <jiaopengzi@outlook.com>
2025-03-22 09:31:25 +0800

    remove excess content

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,11 @@
 -->
 <template>
     <el-menu ellipsis class="el-menu-popper-demo" mode="horizontal" :popper-offset="16" style="max-width: 300px">
-        <Child v-for="(item, key) in testMenuItemMap" :key="key" :menu-item-map="menuItemMap" :menu-item="item" />
+        <Child v-for="(item, key) in testMenuItemMap" :key="key" :menu-item-map="testMenuItemMap" :menu-item="item" />
     </el-menu>
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive } from "vue"
 
 import Child from "./child.vue"
 import { testMenuItemMap } from "./utils"
